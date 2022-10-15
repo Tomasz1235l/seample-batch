5a975b9a0d067638422c3b9f332e3bb8f2b9cbd8
Tomasz1235l <108531577+Tomasz1235l@users.noreply.github.com>
2022-10-15 14:49:07 +0200

    Add files via upload

---
+++ console/shell.bat
@@ -0,0 +1,57 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWGLVpBhie0EEGkSjEVb6b5YO7env5uSA4mglcYI=
+::fBE1pAF6MU+EWGLVpBhie0EEGkSjEVb6b5MV5O317ueC+A0+Dt4Ka4rJyYi6L+0S+V+kXp8333tmvM4fBw9Xbi6tZwMxulJRr3eRMsqgtB/gWFyM4nc3C2Rmk2bUwi4jZbM=
+::fBE1pAF6MU+EWGLVpBhie0EEGkSjEVb6b5MT+uX6+7DH9BlddusrOJOb+buAM+8f7wWsQ58+33hWnYJs
+::fBE1pAF6MU+EWGLVpBhie0EEGkSjEVb6b5QY7OH16KqVp14SQfA8fZyVlPrOD8tz
+::fBE1pAF6MU+EWGLVpBhie0EEGkSjEVb6b4UO5+v+/PnHpEQTXfE3fYu7
+::fBE1pAF6MU+EWGLVpBhie0EFH0SjEVb6b5YO7env5uSA4mglcYI=
+::fBE1pAF6MU+EWGLVpBhie0EFH0SjEVb6b5MV5O317ueC+A0+Dt4Ka4rJyYi6L+0S+V+kXp8333tmudscKBpMfy6HYQw1pVJ2s2mVC5644F6vXkWZqE4oHgU=
+::fBE1pAF6MU+EWGLVpBhie0EFH0SjEVb6b5MT+uX6+7DH9BlddusrOJOb+buAM+8f7wWsQ58+33hWnYJs
+::fBE1pAF6MU+EWGLVpBhie0EFH0SjEVb6b5QY7OH16KqVp14SQfA8fZyVlPrOD8tz
+::fBE1pAF6MU+EWGLVpBhie0EFGESjEVb6b4UO5+v+/PnHpEQTXfE3fYu7
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFC9bQQWRJiuRD68V6dTf4P2JrkIcUPEDbIrJ172AIexe61X0OJ8u2XQUkcgDbA==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFC9bQQWRJiuRD68V6dTf6vmMtkINaOQ2dIrI5qeXM/QW53nnepg+2XZfpNgECRdUMBeza28=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+color 70
+title Terminal
+
+:start
+cd C:/Users/
+cd Tomasz Kizia
+cls
+echo last login at: %time% %date%
+
+:shell
+set /p"command=~$ "
+%command%
+goto shell
